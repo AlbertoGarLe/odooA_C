c76fafc368651362f8640d50fa2187eb72642be3
Sonicx994 <cristian_davidvc03@hotmail.com>
2024-12-11 01:21:57 +0100

    Punto de venta
    Nuevo Modulo - Punto de venta

--- Pag_Boots/js/scripts.js
+++ Pag_Boots/js/scripts.js
@@ -1,54 +1,36 @@
-/*!
-* Start Bootstrap - Agency v7.0.12 (https://startbootstrap.com/theme/agency)
-* Copyright 2013-2023 Start Bootstrap
-* Licensed under MIT (https://github.com/StartBootstrap/startbootstrap-agency/blob/master/LICENSE)
-*/
-//
-// Scripts
-// 
 
-window.addEventListener('DOMContentLoaded', event => {
+window.addEventListener('DOMContentLoaded', () => {
+    const navbarCollapsible = document.querySelector('#mainNav');
+    const navbarToggler = document.querySelector('.navbar-toggler');
+    const responsiveNavItems = document.querySelectorAll('#navbarResponsive .nav-link');
 
     // Navbar shrink function
-    var navbarShrink = function () {
-        const navbarCollapsible = document.body.querySelector('#mainNav');
-        if (!navbarCollapsible) {
-            return;
+    const handleNavbarShrink = () => {
+        if (navbarCollapsible) {
+            navbarCollapsible.classList.toggle('navbar-shrink', window.scrollY > 0);
         }
-        if (window.scrollY === 0) {
-            navbarCollapsible.classList.remove('navbar-shrink')
-        } else {
-            navbarCollapsible.classList.add('navbar-shrink')
-        }
-
     };
 
-    // Shrink the navbar 
-    navbarShrink();
+    // Initialize navbar shrink and add scroll event listener
+    handleNavbarShrink();
+    document.addEventListener('scroll', handleNavbarShrink);
 
-    // Shrink the navbar when page is scrolled
-    document.addEventListener('scroll', navbarShrink);
-
-    //  Activate Bootstrap scrollspy on the main nav element
-    const mainNav = document.body.querySelector('#mainNav');
-    if (mainNav) {
+    // Activate Bootstrap scrollspy if navbar exists
+    if (navbarCollapsible) {
         new bootstrap.ScrollSpy(document.body, {
             target: '#mainNav',
-            rootMargin: '0px 0px -40%',
+            rootMargin: '0px 0px -40%'
         });
-    };
+    }
 
-    // Collapse responsive navbar when toggler is visible
-    const navbarToggler = document.body.querySelector('.navbar-toggler');
-    const responsiveNavItems = [].slice.call(
-        document.querySelectorAll('#navbarResponsive .nav-link')
-    );
-    responsiveNavItems.map(function (responsiveNavItem) {
-        responsiveNavItem.addEventListener('click', () => {
-            if (window.getComputedStyle(navbarToggler).display !== 'none') {
-                navbarToggler.click();
-            }
+    // Collapse responsive navbar on link click if toggler is visible
+    if (navbarToggler && responsiveNavItems.length > 0) {
+        responsiveNavItems.forEach(navItem => {
+            navItem.addEventListener('click', () => {
+                if (getComputedStyle(navbarToggler).display !== 'none') {
+                    navbarToggler.click();
+                }
+            });
         });
-    });
-
+    }
 });
